c6e1e4317a06239ef10e876be9e415e6da16efc3
Serafim Gavrilov <seraf.gavrilov@gmail.com>
2020-02-09 10:53:36 +0300

    add new files to repository

---
+++ script.js
@@ -0,0 +1,39 @@
+let leftBtn = document.createElement('button');
+let rightBtn = document.createElement('button');
+
+leftBtn.innerHTML = '<';
+rightBtn.innerHTML = '>';
+
+leftBtn.classList.add('btn-left');
+rightBtn.classList.add('btn-right');
+
+let item = document.querySelector('.slider-item');
+
+item.append(leftBtn);
+item.append(rightBtn);
+
+let i = 0
+const items = document.querySelectorAll('.item')
+
+const moveSlide = (lambda) => {
+  let nextIndex = i + lambda;
+  
+  
+  if (i + lambda < 0) {
+    nextIndex = items.length - 1;
+  }
+  
+  if (i + lambda > items.length - 1) {
+    nextIndex = 0;
+  }
+  
+  items[nextIndex].classList.remove('disable')
+  items[i].classList.add('disable');
+  i = nextIndex;
+}
+
+rightBtn.addEventListener('click', moveSlide.bind(null, 1));
+
+leftBtn.addEventListener('click', moveSlide.bind(null, -1));
+
+setInterval(moveSlide.bind(null, 1), 2000);
